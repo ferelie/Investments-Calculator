a44804674ca26bc9aeefcc49a690575848c67f61
ferelie <abdallah.yasser0501@gmail.com>
2023-07-30 16:49:47 +0300

    Lifted the state up from Form to App and calculate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import FormComponent from "./components/FormComponent";
 import Header from "./components/Header";
 import ResultsTable from "./components/ResultsTable";
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [results, setResults] = useState(null);
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
-
+    
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    let currentSavings = +userInput["current-savings"]; 
+    const yearlyContribution = +userInput["yearly-contribution"];
     const expectedReturn = +userInput["expected-return"] / 100;
     const duration = +userInput["duration"];
 
@@ -19,7 +21,6 @@ function App() {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
@@ -34,7 +35,7 @@ function App() {
     <div>
       <Header />
 
-      <FormComponent />
+      <FormComponent onCalculate={calculateHandler} />
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,14 +1,44 @@
-const FormComponent = () => {
+import { useState } from "react";
+
+const initialState = ""
+const FormComponent = ({onCalculate}) => {
+  const [inputState, setInputState] = useState(initialState);
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(event);
+    onCalculate(inputState);
+  }
+
+  function resetHandler() {
+    console.log("Reset");
+    setInputState(initialState);
+
+  }
+
+  function changeHandler(event) {
+    const { id, value } = event.target;
+    setInputState((prevInput) => {
+      return { ...prevInput, [id]: value };
+    });
+    console.log(inputState);
+  }
+
   return (
-    <form className="form">
+    <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input onChange={changeHandler} type="number" id="current-savings" value={inputState['current-savings']}/>
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={changeHandler}
+            type="number"
+            id="yearly-contribution"
+            value={inputState['yearly-contribution']}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,15 +46,15 @@ const FormComponent = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input onChange={changeHandler} type="number" id="expected-return" value={inputState['expected-return']} />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input onChange={changeHandler} type="number" id="duration" value={inputState['duration']}/>
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" onClick={resetHandler}className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
